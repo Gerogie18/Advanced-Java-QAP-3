e7c68bb651f29b6543612053bdf87969dcc12562
Gerogie18 <jennlyver@gmail.com>
2025-03-16 19:47:44 -0230

    Create CollegeStudent subclass

---
+++ src/Problem1/CollegeStudent.java
@@ -0,0 +1,45 @@
+package Problem1;
+
+public class CollegeStudent extends Student {
+    protected String myMajor;
+    protected int myYear;
+
+    public CollegeStudent (String name, int age, String gender, int id, float GPA, String major, int year) {
+        super (name, age, gender, id, GPA);
+        this.myMajor = major;
+        if (year >= 1) {
+            this.myYear = year;
+        } else {
+            throw new IllegalArgumentException("Year must be greater than one");
+        }
+    }
+
+    public String getMyMajor() {
+        return myMajor;
+    }
+
+    public void setMyMajor(String major) {
+        this.myMajor = major;
+    }
+
+    public int getMyYear() {
+        return myYear;
+    }
+
+    public void setMyYear(int year) {
+        if (year >= 1) {
+            this.myYear = year;
+        } else {
+            throw new IllegalArgumentException("Year must be greater than one");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CollegeStudent{" +
+                "myMajor='" + myMajor + '\'' +
+                ", myYear=" + myYear +
+                super.toString() +
+                '}';
+    }
+}
