91eb864714e8a008df620f8d23718fcc533b7a72
Gerogie18 <jennlyver@gmail.com>
2025-03-16 19:47:20 -0230

    Create Teacher Class

---
+++ src/Problem1/Teacher.java
@@ -0,0 +1,40 @@
+package Problem1;
+
+public class Teacher extends Person {
+    protected String mySubject;
+    protected double mySalary;
+
+    public Teacher (String name, int age, String gender, String subject, double salary) {
+        super (name, age, gender);
+        this.mySubject = subject;
+        if (salary >= 0) {
+            this.mySalary = salary;
+        }else {
+            throw new IllegalArgumentException("Salary must be larger than 0");
+        }
+    }
+
+    public String getMySubject() {
+        return mySubject;
+    }
+
+    public void setMySubject(String mySubject) {
+        this.mySubject = mySubject;
+    }
+
+    public double getMySalary() {
+        return mySalary;
+    }
+
+    public void setMySalary(double mySalary) {
+        this.mySalary = mySalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "mySubject='" + mySubject + '\'' +
+                ", mySalary=" + mySalary +
+                "} " + super.toString();
+    }
+}
