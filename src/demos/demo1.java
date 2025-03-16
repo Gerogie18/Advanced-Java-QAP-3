e348d5c1e2a448b5b5aee6cf1de68d6e09052438
Gerogie18 <jennlyver@gmail.com>
2025-03-16 20:21:21 -0230

    demo class problem 1

---
+++ src/demos/demo1.java
@@ -0,0 +1,22 @@
+package demos;
+
+import Problem1.*;
+
+public class demo1 {
+    public static void main(String[] args) {
+        Person personExample = new Person("Wanda Smythe", 29, "F");
+        Student studentExample = new Student("Paige Lyndsey", 16, "X", 4378642, 2.7f);
+        Teacher teacherExample = new Teacher("Ted Wong", 32, "M","Chemistry", 50000.0);
+        CollegeStudent collegeStudentExample = new CollegeStudent("Wendy Baker", 21, "F", 123533, 3.5f, "Anthropology", 1);
+
+        System.out.println();
+        System.out.println(personExample);
+        System.out.println();
+        System.out.println(studentExample);
+        System.out.println();
+        System.out.println(teacherExample);
+        System.out.println();
+        System.out.println(collegeStudentExample);
+    }
+
+}
