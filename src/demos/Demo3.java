b25a4f67abae289bd608e932f5f3f1e99ec0fc08
Gerogie18 <jennlyver@gmail.com>
2025-03-16 22:34:32 -0230

    rewrite demo3 and reupload screenshot
    rewrote to make sure the array consists of shapes, like the problem calls for

--- src/demos/Demo3.java
+++ src/demos/Demo3.java
@@ -4,17 +4,17 @@ import Problem3.*;
 
 public class Demo3 {
     public static void main(String[] args) {
-        String[] shapes = new String[] {
-                new Circle(5).toString(),
-                new Ellipse(3, 4).toString(),
-                new EquilateralTriangle(6).toString(),
-                new Triangle(3, 4, 5).toString()
+        Shape[] shapes = new Shape[] {
+                new Circle(5),
+                new Ellipse(3, 4),
+                new EquilateralTriangle(6),
+                new Triangle(3, 4, 5),
         };
 
         //I could have only used the shape string override, but it looked better for each shape to have it's own override.
-        for (String shapePrint : shapes) {
+        for (Shape shape : shapes) {
             System.out.println();
-            System.out.println(shapePrint);
+            System.out.println(shape.toString());
         }
     }
 }
