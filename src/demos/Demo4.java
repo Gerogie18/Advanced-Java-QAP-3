e863e2f1507ed813ee8d860b778740a9cd2490e8
Gerogie18 <jennlyver@gmail.com>
2025-03-16 22:58:44 -0230

    Create demo4 and upload screenshor

--- src/demos/Demo4.java
+++ src/demos/Demo4.java
@@ -3,7 +3,7 @@ package demos;
 import Problem3.*;
 import Problem4.Scalable;
 
-public class Demo3 {
+public class Demo4 {
     public static void main(String[] args) {
         Shape[] shapes = new Shape[] {
                 new Circle(5),
@@ -12,13 +12,19 @@ public class Demo3 {
                 new Triangle(3, 4, 5),
         };
 
-        //I could have only used the shape string override, but it looked better for each shape to have it's own override.
-        System.out.println("Before scaling");
+        System.out.println();
+        System.out.println("Before Scaling");
         for (Shape shape : shapes) {
             System.out.println();
             System.out.println(shape.toString());
-            shape.sca
+            ((Scalable) shape).scale(2.0);
+        }
+
+        System.out.println();
+        System.out.println("After Scaling");
+        for (Shape shape : shapes) {
+            System.out.println();
+            System.out.println(shape.toString());
         }
     }
 }
-
